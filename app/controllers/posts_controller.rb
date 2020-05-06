7d553f97e9cd7bef5a3b093021fe7d1d6a602206
Radicals <cclose27@gmail.com>
2020-05-06 14:57:09 +1000

    setup Devise and user model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :find_post, :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
@@ -71,4 +71,8 @@ class PostsController < ApplicationController
     def post_params
       params.require(:post).permit(:title, :content)
     end
+
+    def find_post
+      @post = Post.find(params[:id])
+     end
 end
