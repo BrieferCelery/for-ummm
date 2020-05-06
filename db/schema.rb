d96e5b6f01909a6ddfb336889bd332af1728155c
Radicals <cclose27@gmail.com>
2020-05-06 14:33:09 +1000

    setup post DB with title and content

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_054345) do
+ActiveRecord::Schema.define(version: 2020_05_06_042856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.text "title"
-    t.text "body"
-    t.integer "replytopost"
-    t.text "postedby"
+    t.string "title"
+    t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
